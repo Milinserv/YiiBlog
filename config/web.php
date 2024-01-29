b559371242598d1226689b0d8ca5eb4db0991f2f
Milinserv <107671454+Milinserv@users.noreply.github.com>
2024-01-29 15:48:27 +0400

    changed the appearance of the URL in the search bar, deactivated cookie checking

--- config/web.php
+++ config/web.php
@@ -13,8 +13,10 @@ $config = [
     ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+
+            'enableCookieValidation' => false,
+
+            'enableCsrfValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +44,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
