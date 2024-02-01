c54cadf4fda1d68aa1b5dc93dd263611f18ff835
Milinserv <107671454+Milinserv@users.noreply.github.com>
2024-02-01 15:15:20 +0400

    change connect to db file

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:8889;dbname=yii2basic',
+    'dsn' => 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=basic',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
